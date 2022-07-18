d12eb06d1221e114399550727ef739d41471afe2
Igor3550 <igormeloigormelo@gmail.com>
2022-07-18 17:44:23 -0300

    versão final com o bonus

--- src/components/Conteudo.js
+++ src/components/Conteudo.js
@@ -1,16 +1,44 @@
+import React from "react";
+
 import PostBox from "./PostBox";
 
-import UserImgTest from './images/naruto.jpeg'
-import LikedImg from './images/itachi.jpeg'
-import Post1 from './images/dog1.png'
+import UserImgTest from './images/naruto.jpeg';
+import LikedImg from './images/itachi.jpeg';
+import Post1 from './images/dog1.png';
 
 const Conteudo = () => {
+
+  const postsList = [
+    {
+      id:1,
+      name:"Narutin",
+      userImg: UserImgTest,
+      postImg: Post1,
+    },
+    {
+      id:2,
+      name:"Narutin",
+      userImg: UserImgTest,
+      postImg: Post1,
+    },
+    {
+      id:3,
+      name:"Narutin",
+      userImg: UserImgTest,
+      postImg: Post1,
+    }
+  ]
+
   return (
 
     <div class="conteudo">
-      <PostBox name="Narutin" userImg={UserImgTest} post={Post1} likedImg={LikedImg}/>
-      <PostBox name="Narutin" userImg={UserImgTest} post={Post1} likedImg={LikedImg}/>
-      <PostBox name="Narutin" userImg={UserImgTest} post={Post1} likedImg={LikedImg}/>
+      {
+        postsList.map((post, key) => {
+          return (
+            <PostBox key={key} id={post.id} name={post.name} userImg={post.userImg} post={post.postImg} likedImg={LikedImg} />
+          )
+        })
+      }
     </div>
 
   )
--- src/components/PostBox.js
+++ src/components/PostBox.js
@@ -1,4 +1,69 @@
+import React from "react";
+/*if(localStorage.getItem('arrayOfIDs') === null){
+    localStorage.setItem('arrayOfIDs' , JSON.stringify([]));
+    arrayOfIDs = localStorage.getItem('arrayOfIDs');
+}else{
+    arrayOfIDs = localStorage.getItem('arrayOfIDs');
+} */
+
+
 const PostBox = (props) => {
+  
+  const [likedPostsIdList, setLikedPostsIdList] = React.useState(getLikedIds());
+
+  const likedListIds = getLikedIds()
+
+  const LikedHeart = () => {
+    return (<ion-icon name="heart" class="likedHeart" onClick={() => {remvLikeClick()}} ></ion-icon>)
+  }
+  const UnlikedHeart = () => {
+    return (<ion-icon name="heart-outline" onClick={() => {likeClick()}}></ion-icon>)
+  }
+
+  function getLikedIds () {
+    let arrayOfIDs;
+    if(localStorage.getItem('arrayOfIDs') === null){
+      localStorage.setItem('arrayOfIDs' , JSON.stringify([]));
+      arrayOfIDs = localStorage.getItem('arrayOfIDs');
+    }else{
+      arrayOfIDs = localStorage.getItem('arrayOfIDs');
+    } 
+    return JSON.parse(arrayOfIDs);
+  }
+
+  console.log(getLikedIds())
+
+  function isLiked (id) {
+    if(likedPostsIdList.includes(id)){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  function likeClick () {
+    likedListIds.push(props.id)
+    console.log(likedListIds)
+    
+    localStorage.setItem('arrayOfIDs' , JSON.stringify(likedListIds));
+
+    setLikedPostsIdList(getLikedIds());
+  }
+
+  function remvLikeClick() {
+
+    likedListIds.map((item, index) => {
+      if(item === props.id){
+        likedListIds.splice(index, 1)
+      }
+    })
+
+    localStorage.setItem('arrayOfIDs' , JSON.stringify(likedListIds));
+
+    setLikedPostsIdList(getLikedIds());
+    
+  }
+
   return (
     <div class="caixa-post">
       <div class="topo">
@@ -9,12 +74,12 @@ const PostBox = (props) => {
         <ion-icon name="ellipsis-horizontal-sharp"></ion-icon>
       </div>
       <div class="post">
-        <img src={props.post}/>
+        <img src={props.post} onClick={() => {likeClick()}} />
       </div>
       <div class="rodape">
         <div class="caixa-rodape-icones">
           <div>
-            <ion-icon name="heart-outline"></ion-icon>
+            {isLiked(props.id) ? <LikedHeart /> : <UnlikedHeart />}
             <ion-icon name="chatbubble-outline"></ion-icon>
             <ion-icon name="paper-plane-outline"></ion-icon>
           </div>
